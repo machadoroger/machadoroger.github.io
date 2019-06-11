81b0e0150772bba85e61775baa159b8b77531051
machadoroger <33077264+machadoroger@users.noreply.github.com>
2019-06-11 16:10:27 -0300

    Update sw.js

--- app/sw.js
+++ app/sw.js
@@ -1,25 +1,91 @@
-var cacheName = 'usersCacheV1';
+// This is the service worker with the Cache-first network
 
-var cacheFiles = [
+const CACHE = "pwabuilder-precache";
+const precacheFiles = [
 	'dist/app.js',
 	'./',
 	'index.html',
-	'assets/js/localForage/localforage.min.js',
-	'assets/js/localForage/localforage-getitems.js',
-	'assets/js/localForage/localforage-setitems.js',
-	'assets/mdl/material.min.css',
-	'assets/mdl/material.min.js'
+	'assets/js/jquery-3.3.1.slim.min.js',
+	'assets/js/bootstrap.min.js',
+    'assets/js/jquery.mask.min.js',
+    'assets/js/jquery.dataTables.min.js',
+	'assets/css/bootstrap.min.css',
+	'assets/css/jquery.dataTables.min.css'
 ];
 
-//Installing
-//Pre-cache App Shell
-self.addEventListener('install', function(event) {
-	console.log("From SW: Install Event");
-	self.skipWaiting();
-	event.waitUntil(
-		caches.open(cacheName)
-		.then(function(cache){
-			return cache.addAll(cacheFiles)
-		})
-	);
+self.addEventListener("install", function (event) {
+  console.log("[PWA Builder] Install Event processing");
+
+  console.log("[PWA Builder] Skip waiting on install");
+  self.skipWaiting();
+
+  event.waitUntil(
+    caches.open(CACHE).then(function (cache) {
+      console.log("[PWA Builder] Caching pages during install");
+      return cache.addAll(precacheFiles);
+    })
+  );
 });
+
+// Allow sw to control of current page
+self.addEventListener("activate", function (event) {
+  console.log("[PWA Builder] Claiming clients for current page");
+  event.waitUntil(self.clients.claim());
+});
+
+// If any fetch fails, it will look for the request in the cache and serve it from there first
+self.addEventListener("fetch", function (event) { 
+  if (event.request.method !== "GET") return;
+
+  event.respondWith(
+    fromCache(event.request).then(
+      function (response) {
+        // The response was found in the cache so we responde with it and update the entry
+
+        // This is where we call the server to get the newest version of the
+        // file to use the next time we show view
+        event.waitUntil(
+          fetch(event.request).then(function (response) {
+            return updateCache(event.request, response);
+          })
+        );
+
+        return response;
+      },
+      function () {
+        // The response was not found in the cache so we look for it on the server
+        return fetch(event.request)
+          .then(function (response) {
+            // If request was success, add or update it in the cache
+            event.waitUntil(updateCache(event.request, response.clone()));
+
+            return response;
+          })
+          .catch(function (error) {
+            console.log("[PWA Builder] Network request failed and no cache." + error);
+          });
+      }
+    )
+  );
+});
+
+function fromCache(request) {
+  // Check to see if you have it in the cache
+  // Return response
+  // If not in the cache, then return
+  return caches.open(CACHE).then(function (cache) {
+    return cache.match(request).then(function (matching) {
+      if (!matching || matching.status === 404) {
+        return Promise.reject("no-match");
+      }
+
+      return matching;
+    });
+  });
+}
+
+function updateCache(request, response) {
+  return caches.open(CACHE).then(function (cache) {
+    return cache.put(request, response);
+  });
+}
